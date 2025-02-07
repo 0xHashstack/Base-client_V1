3242d0889ce8b42ad6b407c23062f24c8bfda0f0
Rupam Shil <rupamshil111@gmail.com>
2025-02-07 14:21:32 +0530

    (feat) minor changes

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function page() {
 	return (
-		<div>
+		<div className='flex items-center justify-center min-h-full'>
 			{/* @ts-expect-error msg */}
 			<appkit-button />
 		</div>
---
+++ note.md
@@ -0,0 +1,3 @@
+1. offers so many features
+2. can modify colors
+3. ui is better than connectkit
