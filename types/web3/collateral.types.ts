b44c543007ff3c5309473f0c074cb33ac2475037
Rupam Shil <rupamshil111@gmail.com>
2025-03-18 12:04:03 +0530

    (feat) fixed token issue

--- types/web3/collateral.types.ts
+++
@@ -1,5 +0,0 @@
-import { HstkToken } from './token.types';
-
-export type CollateralToken = HstkToken & {
-	availableCollateral?: number;
-};
--- types/web3/index.ts
+++ types/web3/index.ts
@@ -1,4 +1,3 @@
 export * from './common-web3.types';
 export * from './web3-core.types';
 export * from './token.types';
-export * from './collateral.types';
